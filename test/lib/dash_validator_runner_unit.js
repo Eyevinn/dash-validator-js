27f844d727ee8a0fa0e4042f0fbd8ab9a9769a56
birme <jonas.birme@eyevinn.se>
2017-01-23 18:15:07 +0100

    Started writing the Dash Validator Runner to validate dynamic playlists

---
+++ test/lib/dash_validator_runner_unit.js
@@ -0,0 +1,48 @@
+// Copyright 2016 Eyevinn Technology. All rights reserved
+// Use of this source code is governed by a MIT License
+// license that can be found in the LICENSE file.
+// Author: Jonas Birme (Eyevinn Technology)
+const MockDate = require("mockdate");
+const DashValidatorRunner = require("../../lib/dash_validator_runner.js");
+const DashManifest = require("../../lib/dash_manifest.js");
+const TestAssetsModule = require("../support/testassets.js");
+
+describe("Dash Validator Runner", () => {
+  beforeEach((done) => {
+    jasmine.clock().install();
+    done();
+  });
+
+  afterEach((done) => {
+    jasmine.clock().uninstall();
+    done();
+  });
+
+  it("can initiate a runner object and verify play head is updating", (done) => {
+    const testAssets = new TestAssetsModule();
+    //MockDate.set("2017-01-23T17:15:00.000000Z");
+    let start = new Date("2017-01-23T16:15:00.000000Z");
+    let dynamicMpd = 
+      testAssets.generateDynamicManifest(new Date("2016-12-23T19:17:47.832317Z"),
+                                         start, 3);
+    let mpd = new DashManifest(dynamicMpd);
+    const validatorRunner = new DashValidatorRunner(mpd);
+    let loopCount = 1;
+
+    function mpdUpdater() {
+      dynamicMpd =
+        testAssets.generateDynamicManifest(new Date("2016-12-23T19:17:47.832317Z"),
+                                           new Date(start.getTime() + (10000 * loopCount++)), 3);                                 
+      mpd = new DashManifest(dynamicMpd);
+      validatorRunner.updateMpd(mpd);
+    }
+
+    validatorRunner.start(3, mpdUpdater).then((result) => {
+      expect(result.ok).toBe(3);
+      done();
+    }).catch(fail).then(done);
+    jasmine.clock().tick(10000);
+    jasmine.clock().tick(10000);
+    jasmine.clock().tick(10000);
+  }); 
+});
--- test/support/testassets.js
+++ test/support/testassets.js
@@ -21,4 +21,113 @@ TestAssets.prototype.getAssetByName = function(name) {
   });
 };
 
+/**
+ * @param {Date} publishTime
+ * @param {Date} startTime
+ * @param {number} segmentLength
+ * @return {Object} Internal MPD object that is used to create a DashManifest object
+ */
+TestAssets.prototype.generateDynamicManifest = function(publishTime, startTime, segmentLength) {
+  const internalMpd = {
+    type: "dynamic",
+    availabilityStartTime: "1970-01-01T00:00:00Z",
+    minBufferTime: "PT10S",
+    timeshiftBufferDepth: "PT1H",
+    maxSegmentDuration: "PT" + segmentLength + "S",
+    publishTime: publishTime.toISOString(),
+  };
+
+  const adaptationSetAudio = {
+    contentType: "audio",
+    mimeType: "audio/mp4",
+    bandwidth: {
+      min: 96000,
+      max: 96000,
+    },
+  };
+  adaptationSetAudio.segmentList = 
+    this.createMockSegmentList(48000, 
+                               startTime, 3600, 
+                               segmentLength, "live-$RepresentationID$-$Time$.dash");
+  adaptationSetAudio.representations = [ 
+    this.createMockRepresentation(adaptationSetAudio.segmentList, 96000, "audio101_swe=96000")
+  ];
+
+  const adaptationSetVideo = {
+    contentType: "video",
+    mimeType: "video/mp4",
+    bandwidth: {
+      min: 254000,
+      max: 254000,
+    },
+  };
+  adaptationSetVideo.segmentList = 
+    this.createMockSegmentList(25, startTime, 3600, 
+                               segmentLength, "live-$RepresentationID$-$Time$.dash");
+  adaptationSetVideo.representations = [
+    this.createMockRepresentation(adaptationSetVideo.segmentList, 254000, "video=254000", 384, 216)
+  ];
+
+  const period = {
+    id: "1",
+    start: "PT0S",
+    baseUrl: "dash/",
+    adaptationSets: [ adaptationSetAudio, adaptationSetVideo ],
+  };
+
+  internalMpd.periods = [ period ];
+
+  return internalMpd;
+};
+
+/**
+ * @param {number} timescale
+ * @param {Date} startTime
+ * @param {number} duration (seconds)
+ * @param {number} segmentLength
+ * @param {string} mediaTemplate
+ */
+TestAssets.prototype.createMockSegmentList = function(timescale, startTime, duration, segmentLength, mediaTemplate) {
+  let segmentList = [];
+
+  for (let i=0; i<(duration/segmentLength); i++) {
+    const segment = {
+      start: (startTime.getTime() / 1000),
+      t: (startTime.getTime() / 1000) * timescale,
+      durationInSeconds: segmentLength,
+      end: (startTime.getTime() / 1000) + duration,
+    };
+    segmentList.push(segment);
+  }
+  return {
+    get timescale() { return timescale; },
+    get media() { return mediaTemplate; },
+    get segments() { return segmentList; },
+  };
+};
+
+TestAssets.prototype.createMockRepresentation = function(segmentList, bandwidth, id, width, height) {
+  const segments_ = [];
+  segmentList.segments.forEach((s) => {
+    segments_.push({
+      t: s.t,
+      start: s.start,
+      end: s.end,
+      durationInSeconds: s.durationInSeconds,
+      uri: uriFromTemplate(segmentList.media, id, s.t),
+    });
+  });
+  return {
+    id: id,
+    get segments() { return segments_; },
+  };
+};
+
+function uriFromTemplate(template, id, time) {
+  let s = template;
+  s = s.replace(/\$RepresentationID\$/, id);
+  s = s.replace(/\$Time\$/, time);
+  return s;
+}
+
 module.exports = TestAssets;
